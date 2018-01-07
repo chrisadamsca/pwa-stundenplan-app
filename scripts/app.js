39a5deef66e9bd16115bc7f7b6c984bd352aacff
Chris Adams <christopher.ca.adams@gmail.com>
2018-01-07 13:56:57 +0100

    API v2.2

--- scripts/app.js
+++ scripts/app.js
@@ -8,37 +8,37 @@
      if (request.status >= 200 && request.status < 300) {
         var days = JSON.parse(request.responseText);
         console.log(days);
-        var timetable = document.getElementById("Timetable");
-        for(var i = 0; i < 7; i++){
-          var courses = days[i].courses;
-          var newH = document.createElement("h2");
-          var newHContent = document.createTextNode(days[i].name);
-          newH.appendChild(newHContent);
-          timetable.appendChild(newH);
-
-          for(var j = 0; j < courses.length; j++){
-            var name = courses[j].name;
-            var time = courses[j].time;
-            var room = courses[j].room;
-
-            var newH = document.createElement("h3");
-            var newHContent = document.createTextNode(name);
-            newH.appendChild(newHContent);
-            timetable.appendChild(newH);
-
-            var newTime = document.createElement("p");
-            var newTimeContent = document.createTextNode("Uhrzeit: " + time);
-            newTime.appendChild(newTimeContent);
-            timetable.appendChild(newTime);
-
-            var newRoom = document.createElement("p");
-            var newRoomContent = document.createTextNode("Raum: " + room);
-            newRoom.appendChild(newRoomContent);
-            timetable.appendChild(newRoom);
-
-          }
-
-        }
+        // var timetable = document.getElementById("Timetable");
+        // for(var i = 0; i < 7; i++){
+        //   var courses = days[i].courses;
+        //   var newH = document.createElement("h2");
+        //   var newHContent = document.createTextNode(days[i].name);
+        //   newH.appendChild(newHContent);
+        //   timetable.appendChild(newH);
+        //
+        //   for(var j = 0; j < courses.length; j++){
+        //     var name = courses[j].name;
+        //     var time = courses[j].time;
+        //     var room = courses[j].room;
+        //
+        //     var newH = document.createElement("h3");
+        //     var newHContent = document.createTextNode(name);
+        //     newH.appendChild(newHContent);
+        //     timetable.appendChild(newH);
+        //
+        //     var newTime = document.createElement("p");
+        //     var newTimeContent = document.createTextNode("Uhrzeit: " + time);
+        //     newTime.appendChild(newTimeContent);
+        //     timetable.appendChild(newTime);
+        //
+        //     var newRoom = document.createElement("p");
+        //     var newRoomContent = document.createTextNode("Raum: " + room);
+        //     newRoom.appendChild(newRoomContent);
+        //     timetable.appendChild(newRoom);
+        //
+        //   }
+        //
+        // }
      } else {
         console.warn(request.statusText, request.responseText);
      }
